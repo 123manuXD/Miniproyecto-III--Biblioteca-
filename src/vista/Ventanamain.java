919ee8b98bc27b59b31feeb234bc891173e0f4b5
Manuel Cardoso <manolocarodoso1501@hotmail.com>
2023-05-25 12:39:09 -0500

    Rediseño de Interfaz (Añadir get, set) funciones

--- src/vista/Ventanamain.java
+++ src/vista/Ventanamain.java
@@ -9,9 +9,9 @@ public class Ventanamain extends JFrame implements ActionListener{
     private JPanel jpLista, jpInformacion, jpPrincipal;
     //botones CRUD
     private JButton btnagregar, btnlimpiar, btneditar, btneliminar;
-    private Decolib im1, im2, im3, im4, im5, im6, im7;
+    private Decolib im1, im2, im3, im4, im5, im6, im7, imlist, imr1, imr2, imr3, imr4;
 
-    private String[] opcionesApartado = {"Usuarios", "Recursos", "Autores", "Generos literarios", "Prestamo"};
+    private String[] opcionesApartado = {"Usuarios", "Recursos", "Autores", "Géneros literarios", "Préstamo"};
     private JComboBox<String> dropApartado = new JComboBox<>(opcionesApartado);
 
     public Ventanamain (){
@@ -38,83 +38,82 @@ public class Ventanamain extends JFrame implements ActionListener{
         jpPrincipal.setBackground(new Color(0, 0, 0, 0));
         add(jpPrincipal);
         
-        im3 = new Decolib("/img/Apartado.png");
-        im3.setBounds((int)606.5, (int)56.5, 277, 57);
-        jpPrincipal.add(im3);
-
-        dropApartado.setBounds((int)886.5, (int)56.5, 257, 57);
-        dropApartado.setOpaque(true);
-        dropApartado.setBackground(new Color(225,225,225));
-        dropApartado.setFont(new Font("arial", Font.ROMAN_BASELINE, 25));
-        jpPrincipal.add(dropApartado);
-
         btnagregar = new JButton("");
-        btnagregar.setBounds(584, 420, 113, 109);
+        btnagregar.setBounds((int)629.5, (int)409.5,232, 67);
         btnagregar.setOpaque(true);
         btnagregar.setContentAreaFilled(false);
         btnagregar.setLayout(null);
         jpPrincipal.add(btnagregar);
 
-        btnlimpiar = new JButton("");
-        btnlimpiar.setBounds((int)733.56, 420, 113, 109);
-        btnlimpiar.setOpaque(true);
-        btnlimpiar.setContentAreaFilled(false);
-        btnlimpiar.setLayout(null);
-        jpPrincipal.add(btnlimpiar);
-
         btneliminar = new JButton();
-        btneliminar.setBounds((int)888.41, 420, 113, 109);
+        btneliminar.setBounds((int)887.5, (int)409.5,232, 67);
         btneliminar.setOpaque(true);
         btneliminar.setContentAreaFilled(false);
         btneliminar.setLayout(null);
         jpPrincipal.add(btneliminar);
 
+        btnlimpiar = new JButton("");
+        btnlimpiar.setBounds((int)629.5, (int)493.5,232, 67);
+        btnlimpiar.setOpaque(true);
+        btnlimpiar.setContentAreaFilled(false);
+        btnlimpiar.setLayout(null);
+        jpPrincipal.add(btnlimpiar);
+
         btneditar = new JButton();
-        btneditar.setBounds((int)1040.22, 420, 113, 109);
+        btneditar.setBounds((int)887.5, (int)493.5,232, 67);
         btneditar.setOpaque(true);
         btneditar.setContentAreaFilled(false);
         btneditar.setLayout(null);
         jpPrincipal.add(btneditar);
         
 
-        im4 = new Decolib("/img/btnadd.png");
-        im4.setBounds(0,0, 113, 109);
+        im4 = new Decolib("/img/botones/btnagregar.png");
+        im4.setBounds(0,0,232, 67);
         btnagregar.add(im4);
 
-        im5 = new Decolib("/img/btnlimpiar.png");
-        im5.setBounds(0,0, 113, 109);
-        btnlimpiar.add(im5);
-
-        im6 = new Decolib("/img/btnEliminar.png");
-        im6.setBounds(0,0, 113, 109);
+        im6 = new Decolib("/img/botones/btneliminar.png");
+        im6.setBounds(0,0,232, 67);
         btneliminar.add(im6);
 
-        im7 = new Decolib("/img/btneditar.png");
-        im7.setBounds(0,0, 113, 109);    
+        im5 = new Decolib("/img/botones/btnLimpiar.png");
+        im5.setBounds(0,0,232, 67);
+        btnlimpiar.add(im5);
+
+        im7 = new Decolib("/img/botones/btneditar.png");
+        im7.setBounds(0,0,232, 67);    
         btneditar.add(im7);
 
         //configurar panel tablas
         jpLista = new JPanel();
         jpLista.setLayout(null);
-        jpLista.setBounds((int)50.03, (int)137.53, (int)518.75 ,(int)464.42);
-        jpLista.setBackground(new Color(232, 246, 239));
+        jpLista.setBounds((int)38.13, (int)118.13, 552 ,494);
+        jpLista.setBackground(new Color(0,0,0,0));
         jpPrincipal.add(jpLista);
+
+        imlist = new Decolib("/img/panelLista.png");
+        imlist.setBounds(0,0, 552, 494);
+        jpLista.add(imlist);
+
+        dropApartado.setBounds((int)185.38, (int)68.34 , (int)338.82, (int)22.52);
+        dropApartado.setOpaque(true);
+        dropApartado.setBackground(new Color(225,225,225));
+        dropApartado.setFont(new Font("arial", Font.ROMAN_BASELINE, 15));
+        jpLista.add(dropApartado); 
 
         jpInformacion = new JPanel();
         jpInformacion.setLayout(null);
         jpInformacion.setBounds((int)605.5, (int)138.5, 541, 248);
-        jpInformacion.setBackground(new Color(232, 246, 239));
+        jpInformacion.setBackground(new Color(0, 0, 0,0));
         jpPrincipal.add(jpInformacion);
 
+        imr1 = new Decolib("/img/panelDatos.png");
+        imr1.setBounds(0, 0, 541, 248);
+        jpInformacion.add(imr1); 
+
         //Fondo del panel princicipal
-        im2 = new Decolib("/img/Fondopanel3.png");
+        im2 = new Decolib("/img/fondopanelfinal.png");
         im2.setBounds(0,0, 1196, 636);
         jpPrincipal.add(im2);
-
-        
-
-
-        //confiugar panel datos
         
     }
     

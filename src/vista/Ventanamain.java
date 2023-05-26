321f8b69fecb7ff649a26fa52a513ee36908c3e2
Manuel Cardoso <manolocarodoso1501@hotmail.com>
2023-05-25 19:31:40 -0500

    final inter

--- src/vista/Ventanamain.java
+++ src/vista/Ventanamain.java
@@ -3,13 +3,14 @@ package vista;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Ventanamain extends JFrame implements ActionListener{
 
-    private JPanel jpLista, jpInformacion, jpPrincipal;
+    private JPanel jpLista, jpInformacion, jpPrincipal, jprplace, jpuser, jprecur, jpautor, jpgnrlite;
     //botones CRUD
-    private JButton btnagregar, btnlimpiar, btneditar, btneliminar;
-    private Decolib im1, im2, im3, im4, im5, im6, im7, imlist, imr1, imr2, imr3, imr4;
+    private JButton btnagregar, btnlimpiar, btneditar, btneliminar, btnusario, btnrecurso, btngnrliterario, btnautor, btnprestamo;
+    private Decolib im1, im2, im3, im4, im5, im6, im7, imlist, imr1, imr2;
 
     private String[] opcionesApartado = {"Usuarios", "Recursos", "Autores", "Géneros literarios", "Préstamo"};
     private JComboBox<String> dropApartado = new JComboBox<>(opcionesApartado);
@@ -65,6 +66,11 @@ public class Ventanamain extends JFrame implements ActionListener{
         btneditar.setContentAreaFilled(false);
         btneditar.setLayout(null);
         jpPrincipal.add(btneditar);
+
+        btngnrliterario = new JButton();
+        btngnrliterario.setOpaque(true);
+        btngnrliterario.setContentAreaFilled(false);
+        btngnrliterario.setLayout(null);
         
 
         im4 = new Decolib("/img/botones/btnagregar.png");
@@ -83,7 +89,7 @@ public class Ventanamain extends JFrame implements ActionListener{
         im7.setBounds(0,0,232, 67);    
         btneditar.add(im7);
 
-        //configurar panel tablas
+        //configurar panell para la lista
         jpLista = new JPanel();
         jpLista.setLayout(null);
         jpLista.setBounds((int)38.13, (int)118.13, 552 ,494);
@@ -100,15 +106,86 @@ public class Ventanamain extends JFrame implements ActionListener{
         dropApartado.setFont(new Font("arial", Font.ROMAN_BASELINE, 15));
         jpLista.add(dropApartado); 
 
+        //Configuracion para el panel donde se tomara la informacion
+
         jpInformacion = new JPanel();
         jpInformacion.setLayout(null);
         jpInformacion.setBounds((int)605.5, (int)138.5, 541, 248);
         jpInformacion.setBackground(new Color(0, 0, 0,0));
         jpPrincipal.add(jpInformacion);
 
+        btnusario = new JButton();
+        btnusario.setBounds(7, 11,101, 29);
+        btnusario.setOpaque(true);
+        btnusario.setContentAreaFilled(false);
+        btnusario.setLayout(null);
+        btnusario.setBorderPainted(false);
+        jpInformacion.add(btnusario);
+
+        btnrecurso = new JButton();
+        btnrecurso.setBounds(117, 11,101, 29);
+        btnrecurso.setOpaque(true);
+        btnrecurso.setContentAreaFilled(false);
+        btnrecurso.setLayout(null);
+        btnrecurso.setBorderPainted(false);
+        jpInformacion.add(btnrecurso);
+
+        btnautor = new JButton();
+        btnautor.setBounds(226, 11,101, 29);
+        btnautor.setOpaque(true);
+        btnautor.setContentAreaFilled(false);
+        btnautor.setLayout(null);
+        btnautor.setBorderPainted(false);
+        jpInformacion.add(btnautor);
+
+        btngnrliterario = new JButton();
+        btngnrliterario.setBounds(335, 11,101, 29);
+        btngnrliterario.setOpaque(true);
+        btngnrliterario.setContentAreaFilled(false);
+        btngnrliterario.setLayout(null);
+        btngnrliterario.setBorderPainted(false);
+        jpInformacion.add(btngnrliterario);
+
+        btnusario.addActionListener(this);
+        btnrecurso.addActionListener(this);
+        btnautor.addActionListener(this);
+        btngnrliterario.addActionListener(this); 
+
+        /* 
+         * Voy a crear un apartado donde creare un panel base para poder remplazar, en otros subpaneles
+         * dependientdo de las necesidades del boton que se presiono, ese metodo se implementara luego 
+        */
+
+        jprplace = new JPanel();
+        jprplace.setBounds(7, 59, 523, 172);
+        jprplace.setLayout(null);
+        jprplace.setBackground(new Color(90, 0, 0));
+        jpInformacion.add(jprplace);
+
+        jpuser = new JPanel();
+        jpuser.setBounds(0, 0, 523, 172);
+        jpuser.setLayout(null);
+        jpuser.setBackground(new Color(150, 0, 0));
+
+        jprecur = new JPanel();
+        jprecur.setBounds(0, 0, 523, 172);
+        jprecur.setLayout(null);
+        jprecur.setBackground(new Color(100, 0, 0));
+
+        jpautor = new JPanel();
+        jpautor.setBounds(0, 0, 523, 172);
+        jpautor.setLayout(null);
+        jpautor.setBackground(new Color(250, 0, 0));
+
+        jpgnrlite = new JPanel();
+        jpgnrlite.setBounds(0, 0, 523, 172);
+        jpgnrlite.setLayout(null);
+        jpgnrlite.setBackground(new Color(50, 0, 0));
+
+        //Fondo panel informacion
         imr1 = new Decolib("/img/panelDatos.png");
         imr1.setBounds(0, 0, 541, 248);
-        jpInformacion.add(imr1); 
+        jpInformacion.add(imr1);
 
         //Fondo del panel princicipal
         im2 = new Decolib("/img/fondopanelfinal.png");
@@ -116,12 +193,31 @@ public class Ventanamain extends JFrame implements ActionListener{
         jpPrincipal.add(im2);
         
     }
-    
-
+     
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent event) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if(event.getSource() == btnusario){
+            System.out.println("Botn de usario");
+            mostralpanel(jpuser);
+        }else if (event.getSource() == btnautor){
+            System.out.println("Botn de autor");
+            mostralpanel(jpautor);
+        }else if (event.getSource() == btnrecurso){
+            System.out.println("Botn de recurso");
+            mostralpanel(jprecur);
+        }else if (event.getSource() == btngnrliterario){
+            System.out.println("Botn de genero literario");
+            mostralpanel(jpgnrlite);
+        }
+
+    }
+
+    private void mostralpanel(JPanel show){
+        jprplace.removeAll();
+        jprplace.add(show);
+        jprplace.revalidate();
+        jprplace.repaint();
     }
 
 }

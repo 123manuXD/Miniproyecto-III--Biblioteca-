766705a624ac9f2b413e2337d7290f54bab8ab41
Juan Rubio <jua0729.jr@gmail.com>
2023-05-29 23:45:12 -0500

    instanciar dao autores

---
+++ src/DAO/AutoresDAO.java
@@ -0,0 +1,18 @@
+/*
+ Archivo: AutoresDAO.java
+ Proyecto III - Biblioteca univalle
+ 29 de mayo de 2023
+
+ Autores:
+  @author Manuel Felipe Cardoso Forero (2027288)
+  @author Juan David Rodriguez Rubio (2025435) 
+ 
+  Creado Unicamente con la intencion de crear una interfaz que implementara AutoresDAO.java 
+ */
+
+package DAO;
+import modelo.*;
+
+public interface AutoresDAO extends ComunDAO<Autores>{
+    
+}
